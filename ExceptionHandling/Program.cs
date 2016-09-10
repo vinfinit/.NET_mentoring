d03d32c1b03a4af8691be86c61845f0f1cc85d2d
vinfinit <artemenko.vladimi@gmail.com>
2016-09-10 20:31:09 +0300

    [+] Exception handling task

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [Serializable]
+    public class StackNetwork : Exception
+    {
+        private readonly int stackTail;
+        public StackNetwork()
+        {
+            
+        }
+
+        public StackNetwork(string message) : base(message)
+        {
+
+        }
+
+        public StackNetwork(string message, int stackTail) : base(message)
+        {
+            this.stackTail = stackTail;
+        }
+
+        public StackNetwork(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+
+        public StackNetwork(string message, int stackTail, Exception inner) : base(message, inner)
+        {
+            this.stackTail = stackTail;
+        }
+    }
+}
