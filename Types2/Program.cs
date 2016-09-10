58f8634a34047730f2788ba826e98a8667a2968a
vinfinit <artemenko.vladimi@gmail.com>
2016-09-11 00:51:05 +0300

    [+] Types2 task

---
+++ Types2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listProduct = new List<Product>();
+            var milk = new Product("milk", 10, 24);
+            listProduct.Add(milk);
+        }
+
+    }
+
+    public struct Product
+    {
+        private string name;
+        private int count;
+        private int cost;
+
+        public Product(string name, int count, int cost)
+        {
+            this.name = name;
+            this.count = count;
+            this.cost = cost;
+        }
+    }
+}
